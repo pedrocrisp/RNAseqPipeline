24ac7c16948bfffd1031f57ce5afbe3a800b3a4b
Kevin Murray <k.d.murray.91@gmail.com>
2013-07-04 16:01:15 +1000

    update file names in make_subread_index.sh

--- 00-misc/make_subread_index.sh
+++ 00-misc/make_subread_index.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-subread-buildindex -o TAIR10_gen TAIR10_allchr.f
+subread-buildindex -o TAIR10_gen TAIR10_chr1.fas  TAIR10_chr3.fas  TAIR10_chr5.fas  TAIR10_chrM.fas TAIR10_chr2.fas  TAIR10_chr4.fas  TAIR10_chrC.fas
