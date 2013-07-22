fe1071e780d933d78a88a7dba7b449653a7d9df6
Kevin Murray <k.d.murray.91@gmail.com>
2013-07-22 16:13:45 +1000

    added subread script

---
+++ 02-align/subread.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# source common function script
+scriptdir="$(dirname $(readlink -f $0))"
+basedir="$scriptdir/.."
+
+source "$basedir/common.sh"
+
+getDefaultOptions $@
+
+subread-align -r "$input" -o "$output" $args
