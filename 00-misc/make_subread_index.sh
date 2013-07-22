f73b197432be26e84537a7e5d7d575eee8ca3fad
Kevin Murray <k.d.murray.91@gmail.com>
2013-07-22 21:40:02 +1000

    update memory usage in make_subread_index.sh

--- 00-misc/make_subread_index.sh
+++ 00-misc/make_subread_index.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-subread-buildindex -o TAIR10_gen TAIR10_chr1.fas  TAIR10_chr3.fas  TAIR10_chr5.fas  TAIR10_chrM.fas TAIR10_chr2.fas  TAIR10_chr4.fas  TAIR10_chrC.fas
+subread-buildindex -M 7000 -o TAIR10_gen TAIR10_chr1.fas  TAIR10_chr3.fas  TAIR10_chr5.fas  TAIR10_chrM.fas TAIR10_chr2.fas  TAIR10_chr4.fas  TAIR10_chrC.fas
