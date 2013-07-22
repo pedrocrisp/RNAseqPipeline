cdd015606b5923d5cfd84480c38ccf7007175516
Kevin Murray <k.d.murray.91@gmail.com>
2013-07-22 15:16:11 +1000

    add fastqc script

---
+++ 01-qc/fastqc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# source common function script
+scriptdir="$(dirname $(readlink -f $0))"
+basedir="$scriptdir/.."
+
+source "$basedir/common.sh"
+getDefaultOptions $@
+
+fastqc $args -o "$output" "$input"
