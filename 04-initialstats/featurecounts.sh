ca52763a5ad309ac673425feca07acd3f5099dd0
Kevin Murray <k.d.murray.91@gmail.com>
2013-07-23 18:57:45 +1000

    added featurecounts script

---
+++ 04-initialstats/featurecounts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# source common function script
+scriptdir="$(dirname $(readlink -f $0))"
+basedir="$scriptdir/.."
+
+source "$basedir/common.sh"
+
+getDefaultOptions $@
+
+featureCounts -i "$input" -o "$output" $args
