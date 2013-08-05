1be1673307ef93f2c64a7f6208c0fc1a234d9655
Kevin Murray <k.d.murray.91@gmail.com>
2013-08-05 18:45:23 +1000

    fix sam/bam input to featureCounts

--- 04-initialstats/featurecounts.sh
+++ 04-initialstats/featurecounts.sh
@@ -8,5 +8,15 @@ source "$basedir/common.sh"
 
 getDefaultOptions $@
 
-bamfile="$(find ${input} -name \*.bam)"
-featureCounts -i "$bamfile" -o "$output/$(basename $bamfile .bam).counts" $args
+bamfile="$(find ${input} -name \*.[bs]am)"
+if [ -n "$(echo $bamfile | grep sam)" ]
+then
+	# sam
+	echo "featureCounts -i \"$bamfile\" -o \"$output/$(basename $bamfile .sam).counts\" $args"
+	featureCounts -i "$bamfile" -o "$output/$(basename $bamfile .sam).counts" $args
+elif [ -n "$(echo $bamfile | grep bam)" ]
+then
+	# bam
+	echo "featureCounts -b -i \"$bamfile\" -o \"$output/$(basename $bamfile .bam).counts\" $args"
+	featureCounts -b -i "$bamfile" -o "$output/$(basename $bamfile .bam).counts" $args
+fi
