76143969417fe15e9f86807661db869d690f9182
Kevin Murray <k.d.murray.91@gmail.com>
2013-08-05 19:11:15 +1000

    fix bam/sam preferening and bad basenameing in featurecounts.sh

--- 04-initialstats/featurecounts.sh
+++ 04-initialstats/featurecounts.sh
@@ -8,15 +8,17 @@ source "$basedir/common.sh"
 
 getDefaultOptions $@
 
-bamfile="$(find ${input} -name \*.[bs]am)"
+bamfile="$(find ${input} -name \*.bam)"
+if [ -z "${bamfile}" ]; then bamfile="$(find ${input} -name \*.sam)"; fi
+
 if [ -n "$(echo $bamfile | grep sam)" ]
 then
 	# sam
-	echo "featureCounts -i \"$bamfile\" -o \"$output/$(basename $bamfile .sam).counts\" $args"
-	featureCounts -i "$bamfile" -o "$output/$(basename $bamfile .sam).counts" $args
+	echo "featureCounts -i \"$bamfile\" -o \"$output/$(basename $output).counts\" $args"
+	featureCounts -i "$bamfile" -o "$output/$(basename $output).counts" $args
 elif [ -n "$(echo $bamfile | grep bam)" ]
 then
 	# bam
-	echo "featureCounts -b -i \"$bamfile\" -o \"$output/$(basename $bamfile .bam).counts\" $args"
-	featureCounts -b -i "$bamfile" -o "$output/$(basename $bamfile .bam).counts" $args
+	echo "featureCounts -b -i \"$bamfile\" -o \"$output/$(basename $output).counts\" $args"
+	featureCounts -b -i "$bamfile" -o "$output/$(basename $output).counts" $args
 fi
