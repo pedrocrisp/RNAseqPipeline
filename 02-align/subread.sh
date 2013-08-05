8fc902cb1b040bc816731e8b0e062f55d4fcfb95
Kevin Murray <k.d.murray.91@gmail.com>
2013-08-05 11:40:08 +1000

    make subread compatible with many input files/do paired end properly

--- 02-align/subread.sh
+++ 02-align/subread.sh
@@ -8,4 +8,26 @@ source "$basedir/common.sh"
 
 getDefaultOptions $@
 
-subread-align -r "$input" -o "$output" $args
+sample=$(basename $input)
+fqFiles="$(ls $input/*.f[aq]*)"
+numFqFiles=$(echo $fqFiles | wc -w)
+
+outsam="align/${sample}/${sample}.sam"
+outbam="align/${sample}/${sample}.bam"
+
+if [ ${numFqFiles} -eq 1 ]
+then
+	subread-align $args -r "$fqFiles" -o "$outsam"
+elif [ ${numFqFiles} -eq 2 ]
+then
+	fq1="$(echo $fqFiles |cut -d ' ' -f 1)"
+	fq2="$(echo $fqFiles |cut -d ' ' -f 2)"
+	subread-align $args -r "${fq1}" -R "${fq2}" -o "$outsam"
+else
+	echo "ERROR: not able to align multiple fq files per pair"
+	echo "fqFiles:"
+	echo "${fqFiles}"
+fi
+
+samtools view -S -b $outsam >$outbam
+
