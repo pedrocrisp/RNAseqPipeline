6b4ab1e12846fc0961d28331fae0f75f995c686d
Kevin Murray <k.d.murray.91@gmail.com>
2013-08-05 13:25:25 +1000

    cleaned up step 1 of my pipeline

--- km_pipeline/1-until_bams.sh
+++ km_pipeline/1-until_bams.sh
@@ -6,17 +6,25 @@ basedir="$scriptdir/../"
 
 source "$basedir/common.sh"
 
+
+###### setup #######
 wsdir="/home/kevin/ws"
 refdir="${wsdir}/refseqs"
 
 sample=$1
 
-# setup
+
+##### Check env ####
 
 if [ ! -d reads ]
 then
 	echo "No ./reads/ directory"
 	exit -1
+fi
+
+if [ -z ${sample} ]
+then
+	echo "Must give sample"
 fi
 
 if [ ! -d reads/${sample} ]
@@ -25,25 +33,28 @@ then
 	exit -1
 fi
 
-# QC
+######## QC ########
 echo "Initial FastQC"
 mkdir -p qc/before/${sample}
 time bash ${basedir}/01-qc/fastqc.sh -i reads/${sample} -o qc/before/${sample} -a ""
-#check_return
 
 echo "Run scythe"
-mkdir -p qcd/${sample}/
-time bash ${basedir}/01-qc/scythe.sh -i reads/${sample} -o qcd/${sample} -a "-p 0.1 -a ${refdir}/adaptors.fa"
-#check_return
+qcstep="scythe"
+mkdir -p qcd/${qcstep}/${sample}/
+time bash ${basedir}/01-qc/scythe.sh -i reads/${sample} -o qcd/${qcstep}/${sample} -a "-p 0.1 -a ${refdir}/adaptors.fa"
+
+# enter any additional qc here
+
+pushd qcd > /dev/null
+ln -s ${qcstep}/${sample}/ ${sample}/
+popd >/dev/null
 
 echo "Run FastQC after all QC steps"
 mkdir -p qc/after/${sample}
-time bash ${basedir}/01-qc/fastqc.sh -i qcd/${sample} -o qc/after/${sample} -a "-q"
-#check_return
+time bash ${basedir}/01-qc/fastqc.sh -i qcd/${sample} -o qc/after/${sample} -a ""
 
 
-# align
+###### align #######
 echo "Align with subread"
 mkdir -p align/${sample}
 time bash ${basedir}/02-align/subread.sh -i qcd/${sample} -o align/${sample} -a "-i ${refdir}/TAIR10_gen/TAIR10_gen"
-#check_return
