861f83832fe488382f338af7feecdb991f1082b6
Kevin Murray <k.d.murray.91@gmail.com>
2013-08-05 16:33:01 +1000

    add pipeline runner

---
+++ km_pipeline/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# source common function script
+scriptdir="$(dirname $(readlink -f $0))"
+basedir="$scriptdir/../"
+
+source "$basedir/common.sh"
+
+
+######### Setup ################
+keyfile=$1
+
+# kefile format: (tab seperated)
+#Ordinal	Sample	<factor1_name> [<factor2_name>]
+
+
+########## Run #################
+# sort keyfile. -n make the header line come at the start, if it starts with a letter
+sort -o $keyfile -k1n $keyfile
+
+samples=$(grep -iv Ordinal < $keyfile | cut -f 2)
+
+echo "Samples are:"
+echo "$samples"
+
+## enter steps ##
+
+# step 1: from raw reads until counts
+mkdir -p ./log/1-until_counts/
+echo $samples |parallel bash ${scriptdir}/1-until_counts.sh {} \>./log/1-until_counts/{}.log 2\>\&1
