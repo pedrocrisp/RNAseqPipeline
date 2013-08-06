fb21fbed61a51cbfaf286b240d242e7940127416
Kevin Murray <k.d.murray.91@gmail.com>
2013-08-06 14:11:16 +1000

    add my incomplete version of an edgeR script

--- 05-diffexpr/edgeR.R
+++ 05-diffexpr/edgeR.R
@@ -2,38 +2,41 @@ source("http://bioconductor.org/biocLite.R")
 # biocLite(c("edgeR")) ## UNCOMMENT TO INSTALL
 
 ## USAGE:
-# edgeR.R --args <commasep_samfiles> <commasep_samplenames> <gfffile>
-
-
-# get arguments
-samfiles <- strsplit(ARGV[1], ",")
-samplenames <- strsplit(ARGV[2], ",")
-gfffile <- ARGV[3]
-
-######## get counts from a bam ############
-# shamelessly pilfered from https://stat.ethz.ch/pipermail/bioc-devel/2012-September/003608.html
-# biocLite(c("Rsubread")) ## UNCOMMENT TO INSTALL
-library(Rsubread)
-
-gfftab <- read.table(gfffile, sep="\t", stringsAsFactors=FALSE)
-txtab <- gfftab[gfftab[,3]=="mRNA",] # change this to exon if you want isoforms
-
-AGIs <- gsub(".+(AT.G\\d{5}\\.\\d).+","\\1",txtab[,9],perl=TRUE)  #get the agi's. dont ask how it works.
-
-annotation <- data.frame(entrezid=as.integer(factor(AGIs)), 
-                   chromosome=txtab$V1, chr_start=txtab$V4, chr_stop=txtab$V5)
-
-# This sorts AGIs by chr position
-ID <- sort(unique(annotation$entrezid))
-AGIs <- AGIs[match(ID, annotation$entrezid)]
-
-
-c_rsub <- featureCounts(SAMfiles=samfiles, annot=annotation, type = "gene")
-counts <- c_rsub$counts
-
-rownames(counts) <-AGIs
-colnames(counts) <-basename(samfiles)
-hist(counts[,1],breaks=1000)
+# edgeR.R --args <commasep_countfiles> <commasep_samplenames> <gfffile>
 
 library(edgeR)
-edgeRUsersGuide()
+
+keyfile.path <- ARGV[1]
+#keyfile.path <- "~/hons-pipeline/km_pipeline/pete-exp277.keyfile"
+keyfile <- read.delim(keyfile.path)
+
+samples <- keyfile$Sample
+countFiles <- paste("count/", samples, "/", samples, ".counts", sep="")
+
+countDFs <- lapply(countFiles, function (fle) read.delim(fle))
+
+# get num genes, == number of rows in first DF
+numGenes <- nrow(countDFs[[1]])
+
+geneNames <- as.character(countDFs[[1]]$geneid)
+geneLengths <- countDFs[[1]]$length
+
+countList <- lapply(countDFs, function (df) df[,3])
+countMatrix <- matrix(unlist(countList), nrow=numGenes)
+sampleGroups <- as.character(keyfile$Treatment)
+dge <- DGEList(counts=countMatrix, group=sampleGroups)
+
+# TODO: DESeq style +1 transform to allow statisical comparison of low abundance -> high abundance transcripts.
+
+dgen <- calcNormFactors(dge, method="TMM")
+dgec <- estimateCommonDisp(dgen)
+dget <- estimateTagwiseDisp(dgec)
+
+# tad hacky, I'll have to come up with a better way of doing this.
+nonControlSamples <- samples[samples!="Control"]
+testPairs <- lapply(nonControlSamples, function (x) c("Control", x))
+testNames <- lapply(testPairs, function (x) paste(x, collapse=".vs."))
+tests <- lapply(testPairs, function (x) exactTest(dget, pair=x))
+names(tests) <- testNames
+
+# incomplete: Need to write results out in a coherent table, like the output of cuffdiff. Also need to do plots.
