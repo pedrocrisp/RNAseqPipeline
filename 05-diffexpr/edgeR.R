e2086204b87c218c5dd9392e06dbcf1d76114c50
Kevin Murray <k.d.murray.91@gmail.com>
2013-08-14 15:46:38 +1000

    simplify input of files in edgeR.R

--- 05-diffexpr/edgeR.R
+++ 05-diffexpr/edgeR.R
@@ -7,36 +7,33 @@ source("http://bioconductor.org/biocLite.R")
 library(edgeR)
 
 keyfile.path <- ARGV[1]
-#keyfile.path <- "~/hons-pipeline/km_pipeline/pete-exp277.keyfile"
 keyfile <- read.delim(keyfile.path)
 
 samples <- keyfile$Sample
 countFiles <- paste("count/", samples, "/", samples, ".counts", sep="")
 
-countDFs <- lapply(countFiles, function (fle) read.delim(fle))
-
-# get num genes, == number of rows in first DF
-numGenes <- nrow(countDFs[[1]])
-
 geneNames <- as.character(countDFs[[1]]$geneid)
-geneLengths <- countDFs[[1]]$length
-
-countList <- lapply(countDFs, function (df) df$nreads)
-countMatrix <- matrix(unlist(countList), nrow=numGenes)
 sampleGroups <- as.character(keyfile$Treatment)
-dge <- DGEList(counts=countMatrix, group=sampleGroups)
+#geneLengths <- countDFs[[1]]$length
+
+dge <- readDGE(
+	       countFiles,
+	       columns=c(1,3),
+	       groups=sampleGroups,
+	       labels=as.character(sampleGroups)
+	       )
 
 # TODO: DESeq style +1 transform to allow statisical comparison of low abundance -> high abundance transcripts.
 
-dgen <- calcNormFactors(dge, method="TMM")
-dgec <- estimateCommonDisp(dgen)
-dget <- estimateTagwiseDisp(dgec)
+dge <- calcNormFactors(dge, method="TMM")
+dge <- estimateCommonDisp(dge)
+dge <- estimateTagwiseDisp(dge)
 
 groups <- as.character(unique(keyfile[,3]))
 nonControlGroups <- groups[groups!="Control"]
 testPairs <- lapply(nonControlGroups, function (x) c("Control", x))
 testNames <- lapply(testPairs, function (x) paste(x, collapse=".vs."))
-tests <- lapply(testPairs, function (x) exactTest(dget, pair=x))
+tests <- lapply(testPairs, function (x) exactTest(dge, pair=x))
 names(tests) <- testNames
 
 for (tst in tests) {
