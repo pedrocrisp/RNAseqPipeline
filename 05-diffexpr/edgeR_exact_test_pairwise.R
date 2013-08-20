073a7fb564c777b665540b1f7705a6e13a0570e9
Kevin Murray <k.d.murray.91@gmail.com>
2013-08-20 12:28:27 +1000

    misc refactoring etc in edgeR pairwise

--- 05-diffexpr/edgeR_exact_test_pairwise.R
+++ 05-diffexpr/edgeR_exact_test_pairwise.R
@@ -1,11 +1,15 @@
-source("http://bioconductor.org/biocLite.R")
-# biocLite(c("edgeR")) ## UNCOMMENT TO INSTALL
+## UNCOMMENT TO INSTALL
+# source("http://bioconductor.org/biocLite.R")
+# biocLite(c("edgeR"))
+
+library(edgeR)
+library(multicore)
+library(reshape2)
 
 ## USAGE:
 # edgeR.R --args <keyfile>
-library(edgeR)
-library(multicore)
 ARGV <- commandArgs(trailingOnly=TRUE)
+if (length(ARGV) < 1) {stop("USAGE: edgeR.R --args <keyfile>"); q(status=-1)}
 
 allpairs <- FALSE
 
@@ -16,28 +20,27 @@ allpairs <- FALSE
 keyfile.path <- ARGV[1]
 keyfile <- read.delim(keyfile.path)
 samples <- keyfile$Sample
-analysisName <- unlist(strsplit(rev(unlist(strsplit(keyfile.path, "/")))[1], "\\."))[1]
+analysis.name <- unlist(strsplit(rev(unlist(strsplit(keyfile.path, "/")))[1], "\\."))[1]
 rm(keyfile.path)
 
 ################################################################################
 ######################           Data Entry         ############################
 ################################################################################
 
-countFiles <- paste("count/", samples, "/", samples, ".counts", sep="")
+count.files <- paste("count/", samples, "/", samples, ".counts", sep="")
 
-sampleGroups <- as.character(paste(keyfile[,3:ncol(keyfile)]))
-sampleGroups
+sample.groups <- as.character(paste(keyfile[,3:ncol(keyfile)]))
 #geneLengths <- countDFs[[1]]$length
 
 dge <- readDGE(
-  countFiles,
+  count.files,
   columns=c(1,3),
-  group=sampleGroups,
+  group=sample.groups,
   labels=as.character(samples)
 )
-geneNames <- as.character(rownames(dge$counts))
+gene.names <- as.character(rownames(dge$counts))
 
-rm(countFiles)
+rm(count.files)
 
 ################################################################################
 ######################      DGE Normalisation       ############################
@@ -55,6 +58,8 @@ old.dge <- dge
 dge <- old.dge[loci.2.keep,]
 dge$samples$lib.size <- colSums(dge$counts)
 
+gene.names.keep <- gene.names[loci.2.keep]
+
 dge <- calcNormFactors(dge, method="TMM")
 dge <- estimateCommonDisp(dge)
 dge <- estimateTagwiseDisp(dge)
@@ -68,49 +73,60 @@ rm(num.samples.with.enough.reads)
 ################################################################################
 
 
-groups <- unique(sampleGroups)
+groups <- unique(sample.groups)
 if(allpairs) {
   testPairs <- combn(groups, 2)
 } else {
-  nonControlGroups <- groups[2:length(groups)]
-  testPairs <- lapply(nonControlGroups, function (x) c("Control", x))
+  non.control.groups <- groups[2:length(groups)]
+  test.pairs <- lapply(non.control.groups, function (x) c("Control", x))
 }
-testNames <- lapply(testPairs, function (x) paste(x, collapse=".vs."))  
-tests <- mclapply(testPairs, function (x) exactTest(dge, pair=x))
-names(tests) <- testNames
-
+test.names <- lapply(test.pairs, function (x) paste(x, collapse=".vs."))
+tests <- mclapply(test.pairs, function (x) exactTest(dge, pair=x))
+names(tests) <- test.names
+names(tests)
 
 ################################################################################
 ######################         Write Results        ############################
 ################################################################################
 
+short.sample.names <- sapply(
+  strsplit(as.character(samples),  "_"),
+  function (l) paste(l[4:5], collapse="_")
+  )
+
+nice.sample.names <- paste(sample.groups, rep(1:3,length(groups)), sep=".")
+
 # global plots
 pdf(paste0(analysisName, ".pdf"))
 plotBCV(dge, main=analysisName)
-plotMDS(dge, main=analysisName)
+plotMDS(dge,
+        main=analysisName,
+        labels=short.sample.names,
+        col=rainbow(length(groups))
+        )
 dev.off()
 
 # write exact test tables out
 xf <-  4 # fold change at which lines are ruled
 for (tst in tests) {
-	testName <- paste(tst$comparison, collapse=".VS.")
-	baseName <-paste0("de/", testName)
+	test.name <- paste(tst$comparison, collapse=".VS.")
+	base.name <-paste0("de/", test.name)
 
 	decision <- decideTestsDGE(tst, p=0.05)
-	detags <- geneNames[as.logical(decision)]
+	detags <- gene.names.keep[as.logical(decision)]
 	table <- tst$table
-	table$geneid <- geneNames
+	table$geneid <- gene.names.keep
 	row.names(table) <- NULL
-	# next line doesn't work as it should. Row names are not changed after sort.
+	# next line doesn't work as it should. Row names are not changed after sort.           
 	table <- table[with(table, order(PValue)),]
-	write.csv(table, paste0(baseName, ".csv"))
+	write.csv(table, paste0(base.name, ".csv"))
 
 	# plots
-	pdf(paste0(baseName, "_smear.pdf"))
+	pdf(paste0(base.name, "_smear.pdf"))
 	plotSmear(
 		  dge,
 		  de.tags=detags,
-		  main=testName,
+		  main=test.name,
 		  sub=paste("lines indicate", xf, "fold change")
 		  )
 	abline(h=c(-log2(xf), log2(xf)), col="blue")
