bf8d3058388361a02f223613f4d976d87372dfb9
Kevin Murray <k.d.murray.91@gmail.com>
2013-09-03 19:52:23 +1000

    update tophat for paired end compatibility

--- 02-align/tophat.sh
+++ 02-align/tophat.sh
@@ -8,4 +8,19 @@ source "$basedir/common.sh"
 
 getDefaultOptions $@
 
-tophat --solexa-quals --library-type fr-unstranded $args -o "$output/tophat_out" "$input"
+if [ ${numFqFiles} -eq 1 ]
+then
+	tophat $args  -o "$output/tophat_out" "$fqFiles"
+elif [ ${numFqFiles} -eq 2 ]
+then
+	fq1="$(echo $fqFiles |cut -d ' ' -f 1)"
+	fq2="$(echo $fqFiles |cut -d ' ' -f 2)"
+	tophat $args -o "$output/tophat_out" "${fq1}" "${fq2}"
+else
+	echo "ERROR: not able to align multiple fq files per pair"
+	echo "fqFiles:"
+	echo "${fqFiles}"
+fi
+
+# link to keep bam position compatible w/ other aligners
+ln -s "$output/tophat_out/accepted_hits.bam" "$output/$(basename $output).bam"
