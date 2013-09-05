fcd4297b8a08385e7b5edeed1a23321c68285e54
Kevin Murray <k.d.murray.91@gmail.com>
2013-09-05 19:19:04 +1000

    add make_igv_bams pl

---
+++ make_igv_bams/make_igv_bams.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# source common function script
+scriptdir="$(dirname $(readlink -f $0))"
+basedir="$scriptdir/../"
+
+source "$basedir/common.sh"
+
+
+###### setup #######
+wsdir="$(readlink -f ~/ws)"
+refdir="${wsdir}/refseqs"
+
+sample=$1
+
+
+##### Check env ####
+
+if [ ! -d align ]
+then
+	echo "No ./align/ directory"
+	exit -1
+fi
+
+if [ -z "${sample}" ]
+then
+	echo "Must give sample"
+fi
+
+if [ ! -d "align/${sample}" ]
+then
+	echo "Error: sample '${sample}' does not exist"
+	exit -1
+fi
+
+###### align #######
+echo "Convert bams"
+time bash ${basedir}/02-align/subread.sh -i align/${sample} -o align/${sample} -a ""
---
+++ make_igv_bams/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# source common function script
+scriptdir="$(dirname $(readlink -f $0))"
+basedir="$scriptdir/../"
+
+source "$basedir/common.sh"
+
+
+######### Setup ################
+keyfile=$1
+
+# kefile format: (tab seperated)
+#Ordinal	Sample	<factor1_name> [<factor2_name>]
+
+
+########## Run #################
+# sort keyfile. -n make the header line come at the start, if it starts with a letter
+sort -o $keyfile -k1n $keyfile
+
+function getSamples() {
+	grep -iv Ordinal < $keyfile | cut -f 2
+}
+
+echo "Samples are:"
+echo "$(getSamples)"
+
+## enter steps ##
+
+mkdir -p ./log/make_igv_bams/
+getSamples |parallel bash ${scriptdir}/make_igv_bams.sh {} \>./log/make_igv_bams/{}.log 2\>\&1
