0c4cc1edc586e85b30f9e307c13717d7915298be
Kevin Murray <k.d.murray.91@gmail.com>
2013-09-09 13:58:24 +1000

    add subjunc aligner script

---
+++ 02-align/subjunc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# source common function script
+scriptdir="$(dirname $(readlink -f $0))"
+basedir="$scriptdir/.."
+
+source "$basedir/common.sh"
+
+getDefaultOptions $@
+
+sample=$(basename $input)
+fqFiles="$(ls $input/*.f[aq]*)"
+numFqFiles=$(echo $fqFiles | wc -w)
+
+outsam="align/${sample}/${sample}.sam"
+outbam="align/${sample}/${sample}.bam"
+
+if [ ${numFqFiles} -eq 1 ]
+then
+	subread-align $args -r "$fqFiles" -o "$outsam"
+elif [ ${numFqFiles} -eq 2 ]
+then
+	fq1="$(echo $fqFiles |cut -d ' ' -f 1)"
+	fq2="$(echo $fqFiles |cut -d ' ' -f 2)"
+	subjunc $args -r "${fq1}" -R "${fq2}" -o "$outsam"
+else
+	echo "ERROR: not able to align multiple fq files per pair"
+	echo "fqFiles:"
+	echo "${fqFiles}"
+fi
+
+samtools view -S -u $outsam | samtools sort -m 2G -f - $outbam
+samtools index $outbam
+rm -v ${outsam}
