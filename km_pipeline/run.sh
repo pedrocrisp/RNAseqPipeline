d8d5c08117b8ab2ee5a917b2f164e5d6599e24d6
Kevin Murray <k.d.murray.91@gmail.com>
2013-09-09 14:57:16 +1000

    cat script files into logs in km_pipeline

--- km_pipeline/run.sh
+++ km_pipeline/run.sh
@@ -25,11 +25,16 @@ function getSamples() {
 echo "Samples are:"
 echo "$(getSamples)"
 
+cat $0
 ## enter steps ##
 
 # step 1: from raw reads until counts
 mkdir -p ./log/1-until_counts/
-getSamples |parallel bash ${scriptdir}/1-until_counts.sh {} \>./log/1-until_counts/{}.log 2\>\&1
+cat ${scriptdir}/1-until_counts.sh
+getSamples |parallel "bash ${scriptdir}/1-until_counts.sh {} >./log/1-until_counts/{}.log 2>&1"
 
+
+# step 2: differential expression
 mkdir -p ./de
+cat ${scriptdir}/edgeR-kmhons.R
 R -f ${scriptdir}/edgeR-kmhons.R --args $keyfile >./log/de.log
