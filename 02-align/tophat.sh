053b67ea097036ba4bef56c69837951f6df87dd8
Kevin Murray <k.d.murray.91@gmail.com>
2013-09-16 12:09:51 +1000

    fix a few bugs in tophat.sh wrapper scripts

--- 02-align/tophat.sh
+++ 02-align/tophat.sh
@@ -6,24 +6,36 @@ basedir="$scriptdir/.."
 
 source "$basedir/common.sh"
 
+# order of arguments to tophat.sh is important. Ref base must be given to -a, without an argument, and must be the last argument
 getDefaultOptions $@
+
+sample=$(basename $input)
+fqFiles="$(ls $input/*.f[aq]*)"
+numFqFiles=$(echo $fqFiles | wc -w)
+
+outbam="${output}/${sample}" # no .bam, as samtools sort -f has a bug.
+tophatbam="${output}/tophat_out/accepted_hits.bam"
 
 if [ ${numFqFiles} -eq 1 ]
 then
 	echo tophat $args  -o "$output/tophat_out" "$fqFiles"
-	tophat $args  -o "$output/tophat_out" "$fqFiles"
+	tophat -o "$output/tophat_out" $args "$fqFiles"
 elif [ ${numFqFiles} -eq 2 ]
 then
 	fq1="$(echo $fqFiles |cut -d ' ' -f 1)"
 	fq2="$(echo $fqFiles |cut -d ' ' -f 2)"
-	echo tophat $args -o "$output/tophat_out" "${fq1}" "${fq2}"
-	tophat $args -o "$output/tophat_out" "${fq1}" "${fq2}"
+	echo tophat -o "$output/tophat_out" $args "${fq1}" "${fq2}"
+	tophat -o "$output/tophat_out" $args "${fq1}" "${fq2}"
 else
 	echo "ERROR: not able to align multiple fq files per pair"
 	echo "fqFiles:"
 	echo "${fqFiles}"
+	exit 1
 fi
 
-# link to keep bam position compatible w/ other aligners
-echo ln -s "$output/tophat_out/accepted_hits.bam" "$output/$(basename $output).bam"
-ln -s "$output/tophat_out/accepted_hits.bam" "$output/$(basename $output).bam"
+
+echo "samtools sort -m 2G ${tophatbam} $outbam
+samtools index ${outbam}.bam"
+
+samtools sort -m 2G ${tmpbam} $outbam
+samtools index ${outbam}.bam
