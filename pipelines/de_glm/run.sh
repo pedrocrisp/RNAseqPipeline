82bf1d10e69c1da42119ed4d93e3a8e1706adf11
Kevin Murray <k.d.murray.91@gmail.com>
2013-10-02 19:26:54 +1000

    update de_glm runner to new standards

--- pipelines/de_glm/run.sh
+++ pipelines/de_glm/run.sh
@@ -6,22 +6,37 @@ basedir="$scriptdir/../../"
 
 source "$basedir/common.sh"
 
+alias timestamp='date +%Y%m%d-%H%M%S'
+alias usage="echo 'run.sh <keyfile> <r_config_file>'"
+
 
 ######### Setup ################
 keyfile=$1
+rconffile=$2
 
 # kefile format: (tab seperated)
 #Ordinal	Sample	<factor1_name> [<factor2_name>]
 
-
-##### Check env ####
-
 if [ ! -d count ]
 then
 	echo "No ./count directory"
+	usage
 	exit -1
 fi
 
+if [ ! -r $keyfile ]
+then
+	echo "Must provide kefile"
+	usage
+	exit -1
+fi
+
+if [ ! -r $rconffile ]
+then
+	echo "Must provide R configuration file"
+	usage
+	exit -1
+fi
 
 ########## Run #################
 
@@ -33,4 +48,4 @@ cat $0
 mkdir -p ./de
 script="${basedir}/edgeR_glm_multifactor.R"
 cat $script
-R -f ${script} --args $keyfile ${scriptdir}/edgeR_kmhons.R >./log/de.log
+R -f ${script} --args $keyfile $rconffile >./log/de.`timestamp`.log
