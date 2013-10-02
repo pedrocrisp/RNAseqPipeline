dc607fa0ee7f5aaf900c8d9b987d413d40552d81
Kevin Murray <k.d.murray.91@gmail.com>
2013-10-02 19:33:46 +1000

    update rob_pl runner to new standards

--- pipelines/rob_pl/run.sh
+++ pipelines/rob_pl/run.sh
@@ -6,12 +6,22 @@ basedir="$scriptdir/../../"
 
 source "$basedir/common.sh"
 
+alias timestamp='date +%Y%m%d-%H%M%S'
+alias usage="echo 'run.sh <keyfile>'"
+
 
 ######### Setup ################
 keyfile=$1
 
 # kefile format: (tab seperated)
 #Ordinal	Sample	<factor1_name> [<factor2_name>]
+
+if [ ! -r $keyfile ]
+then
+	echo "Must provide kefile"
+	usage
+	exit -1
+fi
 
 
 ########## Run #################
@@ -25,8 +35,11 @@ function getSamples() {
 echo "Samples are:"
 echo "$(getSamples)"
 
+cat $0
+
 ## enter steps ##
 
 # step 1: from raw reads until counts
 mkdir -p ./log/until_counts/
-getSamples |parallel bash ${scriptdir}/until_counts.sh {} \>./log/until_counts/{}.log 2\>\&1
+script=${scriptdir}/until_counts.sh
+getSamples |parallel bash ${script} {} \>./log/until_counts.`timestamp`.log 2\>\&1
