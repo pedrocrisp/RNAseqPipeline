c6ce8b90fc27cacc1404acc0d51a2db679095624
Kevin Murray <k.d.murray.91@gmail.com>
2013-10-04 20:40:23 +1000

    add stranded subread pipeline
    Add a pipeline to align stranded libraries.

---
+++ pipelines/aln_subread_stranded/run.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# source common function script
+scriptdir="$(dirname $(readlink -f $0))"
+basedir="$scriptdir/../../"
+
+source "$basedir/common.sh"
+
+alias timestamp='date +%Y%m%d-%H%M%S'
+alias usage="echo 'run.sh <keyfile>'"
+
+
+######### Setup ################
+keyfile=$1
+
+# kefile format: (tab seperated)
+#Ordinal	Sample	<factor1_name> [<factor2_name>]
+
+if [ ! -r $keyfile ]
+then
+	echo "Must provide kefile"
+	usage
+	exit -1
+fi
+
+
+########## Run #################
+# sort keyfile. -n make the header line come at the start, if it starts with a letter
+sort -o $keyfile -k1n $keyfile
+
+function getSamples() {
+	grep -iv Ordinal < $keyfile | cut -f 2
+}
+
+echo "Samples are:"
+echo "$(getSamples)"
+
+cat $0
+
+## enter steps ##
+
+# step 1: from raw reads until counts
+mkdir ./log/until_counts/
+script=${scriptdir}/until_counts.sh
+cat $script
+getSamples |parallel bash ${script} {} \>./log/until_counts.`timestamp`/{}.log 2\>\&1
