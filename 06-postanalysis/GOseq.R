ccc7cdab6968ba773796607807ed7c5ee7f00839
Kevin Murray <k.d.murray.91@gmail.com>
2013-10-06 00:42:49 +1000

    midnight version of GOseq.R

---
+++ 06-postanalysis/GOseq.R
@@ -0,0 +1,79 @@
+library(goseq)
+library(multicore)
+
+ARGV <- commandArgs(trailingOnly=TRUE)
+ARGV <- c("de/kevin-hons-277-R60/kevin-hons-277-R60_fdr.csv")
+if (length(ARGV) < 1) {
+  stop("USAGE: GOseq.R --args <fdr.table> [<goslim mapping> [<gff>]]")
+  q(status=-1)
+}
+
+out.base <- "go/"
+dir.create(out.base, showWarnings=F)
+
+fdr.path <- ARGV[1]
+
+tair <- "ftp://ftp.arabidopsis.org/home/tair/"
+if (length(ARGV) == 2) {
+  go.path <- ARGV[2]
+} else {
+  go.path <- paste0(tair, "Ontologies/Gene_Ontology/ATH_GO_GOSLIM.txt")
+}
+
+if (length(ARGV) == 3) {
+  gff.path <- ARGV[3]
+} else {
+  gff.path <- paste0(tair, "Genes/TAIR10_genome_release/TAIR10_gff3/TAIR10_GFF3_genes.gff")
+}
+
+# grab go table from tair
+go <- read.delim(go.path, header=F)
+names(go) <- c(
+  "locusName", "TAIRAccession", "AGI", "GOrelationship", "GOterm",
+  "GOID", "TAIRKeywordID", "Aspect", "GOslimTerm", "EvidenceCode",
+  "EvidenceDesc", "EvidenceWith", "Reference", "Annotator", "Date"
+  )
+go.goseq <- go[,c("AGI", "GOID")]
+
+# gff preparation
+gff <- read.delim(gff.path, header=F)
+gff <- gff[gff[,3] == "gene",]
+
+agis <- sapply(
+  as.character(gff[,9]),
+  function(s)
+    rev(unlist(strsplit(s, "=")))[1]
+  )
+names(agis) <- NULL
+gene.lengths <- gff[,5] - gff[,4] + 1
+names(gene.lengths) <- agis
+
+
+fdr <- read.csv(fdr.path)
+rownames(fdr) <- as.character(fdr[,1])
+fdr <- as.matrix(fdr[,2:length(fdr)])
+
+summary(fdr)
+
+all.genes <- rownames(fdr)
+gene.lengths <- gene.lengths[match(all.genes, names(gene.lengths))]
+
+goTestIndex <- function(tn) {
+  tst <- fdr[,tn]
+  test.name <- colnames(fdr)[tn]
+  de.genes <- as.integer(tst < 0.05)
+  names(de.genes) <- all.genes
+  pwf <- nullp(de.genes, "tair10", id=all.genes, bias.data=gene.lengths)
+  go.all <- goseq(pwf, "tair10", gene2cat=go.goseq)
+  this.go <- go[match(go.all$category, as.character(go$GOID)),c(4:7)]
+  go.all$GOrelationship <- this.go$GOrelationship
+  go.all$GOterm <- this.go$GOterm
+  go.all$sig <- go.all$over_represented_pvalue < 0.05
+  row.names(go.all) <- NULL
+  
+  out.name <- paste0(out.base, test.name, ".goall.csv")
+  print(out.name)
+  write.csv(go.all, out.name, row.names=F)
+}
+
+GA <- mclapply(1:ncol(fdr), goTestIndex)
