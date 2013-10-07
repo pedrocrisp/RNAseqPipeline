16ac786e1f655856f89468b73be6ef62da4aa63b
Kevin Murray <k.d.murray.91@gmail.com>
2013-10-07 18:20:45 +1100

    forgot that htseq doesn't like bams
    You've got to samtools sort and view
    per:
    http://seqanswers.com/forums/showthread.php?t=24814

--- 04-initialstats/htseq-count.sh
+++ 04-initialstats/htseq-count.sh
@@ -13,5 +13,5 @@ bamfile="$(findBAMFiles ${input})"
 gffFile="$(echo $args| sed -e 's/ /\n/g' | grep -A 1 -- '-a' | grep -v -- '-a')"
 args=$(echo $args |sed -e 's/ /\n/g' |grep -v -- -a |grep -v -- $gffFile  | tr '\n' ' ')
 
-echo "htseq-count $args $bamfile $gffFile > \"$output/$(basename $output).counts\""
-htseq-count $args $bamfile $gffFile > "$output/$(basename $output).counts"
+echo "samtools sort -on $bamfile - | samtools view -h - |htseq-count $args - $gffFile > \"$output/$(basename $output).counts\""
+samtools sort -on $bamfile - | samtools view -h - |htseq-count $args - $gffFile > "$output/$(basename $output).counts"
